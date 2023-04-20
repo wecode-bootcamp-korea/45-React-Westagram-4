a09762b72e944c8b9686b2222f94ed9d8f0d089e
yoohmoon <ansekfzhd5455@naver.com>
2023-04-20 22:11:49 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/yoohyunMoon/Login/LoginYoohyun.js
+++ src/pages/yoohyunMoon/Login/LoginYoohyun.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LoginYoohyun.scss';
 
 const LoginYoohyun = () => {
   const navigate = useNavigate();
+
+  let [userId, setUserId] = useState(null);
+  let [userPw, setUserPw] = useState(null);
+
+  const saveUserId = event => {
+    // console.log('event.target', event.target);
+    userId = event.target.value;
+    // console.log('userId', userId);
+  };
+
+  const saveUserPw = event => {
+    userPw = event.target.value;
+  };
 
   return (
     <div className="loginYoohyun">
@@ -18,10 +31,16 @@ const LoginYoohyun = () => {
                 type="text"
                 minLength="1"
                 placeholder="전화번호, 사용자 이름 또는 이메일"
+                onChange={saveUserId}
               />
             </div>
             <div>
-              <input type="password" minlength="1" placeholder="비밀번호" />
+              <input
+                type="password"
+                minLength="1"
+                placeholder="비밀번호"
+                onChange={saveUserPw}
+              />
             </div>
             <div>
               <button
