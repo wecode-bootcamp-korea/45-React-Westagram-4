f3fcd853b7e6884ac38038ba5cfa34984a677327
yoohmoon <ansekfzhd5455@naver.com>
2023-04-21 20:20:28 +0900

    1-2 중간 커밋: 버튼 색상 변경 완료

--- src/pages/yoohyunMoon/Login/LoginYoohyun.js
+++ src/pages/yoohyunMoon/Login/LoginYoohyun.js
@@ -5,22 +5,53 @@ import './LoginYoohyun.scss';
 const LoginYoohyun = () => {
   const navigate = useNavigate();
 
-  let [userId, setUserId] = useState(null);
-  let [userPw, setUserPw] = useState(null);
-
-  const saveUserId = event => {
+  let [userId, setUserId] = useState('');
+  let [userPw, setUserPw] = useState('');
+  const saveUserId = e => {
     // console.log('event.target', event.target);
-    userId = event.target.value;
+    setUserId(e.target.value);
     // console.log('userId', userId);
+
+    /*    if (userId.includes('@')) {
+      console.log('saveUserId 에 @가 들어가요 : true');
+      setUserId(userId);
+      // setBtnState(true);
+      activateBtn();
+      // return userId;/
+    }  */
   };
+  console.log('userId??? ', userId);
 
   const saveUserPw = event => {
-    userPw = event.target.value;
+    // userPw = event.target.value;
+    setUserPw(event.target.value);
+
+    /* if (userPw.length >= 5) {
+      // console.log('userPw가 5이상이에용 : true', userPw);
+      setUserPw(userPw);
+      activateBtn();
+      // return userPw;
+    } else {
+      return false;
+    } */
   };
+  // console.log(saveUserPw);
+  console.log('userPw??? ', userPw);
 
   // 로그인 버튼 활성화(validation)
   const [btnState, setBtnState] = useState(false);
-  const activateBtn = () => {};
+  const activateBtn = () => {
+    const validId = userId && userId.includes('@');
+    const validPw = userPw && userPw.length >= 5;
+    validId && validPw ? setBtnState(true) : setBtnState(false);
+    /*   if (userId.includes('@') && userPw.length >= 5) {
+      setBtnState(true);
+    } else {
+      setBtnState(false);
+    } */
+    /* saveUserId && saveUserPw ? setBtnState(true) : setBtnState(false); */
+    // userId !== null && userPw !== null ? setBtnState(true) : setBtnState(false);
+  };
 
   return (
     <div className="loginYoohyun">
@@ -48,11 +79,26 @@ const LoginYoohyun = () => {
             </div>
             <div>
               <button
-                className={`${!btnState} ? activate : unactivate`}
+                // className={`${btnState === true} ? activate : unactivate`}
+                // className={userPw.length >= 5 ? 'activate' : 'unactivate'}
+                // className={btnState === true ? 'activate' : 'unactivate'}
+                /*   className={
+                  `${saveUserId && saveUserPw}` ? 'activate' : 'unactivate'
+                } */
+                className={
+                  userId.includes(`@`) && userPw.length >= 5
+                    ? `activate`
+                    : `unactivate`
+                }
                 onClick={() => {
                   navigate('/main-yoohyun');
                 }}
-                onChange={activateBtn}
+                // onChange={activateBtn}
+                /*  onChange={
+                  userId !== null && userPw !== null
+                    ? setBtnState(true)
+                    : setBtnState(false)
+                } */
               >
                 로그인
               </button>
