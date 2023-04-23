2da707fba46539749caa9899c666da575dee7411
yoohmoon <ansekfzhd5455@naver.com>
2023-04-23 20:33:21 +0900

    Modify : commentItem.scss 스타일링 수정

--- src/pages/yoohyunMoon/Main/components/CommentItem.js
+++ src/pages/yoohyunMoon/Main/components/CommentItem.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './CommentItem.scss';
 
+const COMMENTER_ID = 'wecode';
+
 function CommentItem({ comment }) {
   return (
     <li className="commentItem">
       <div className="commentItemDetail">
-        <span>wecode</span>
+        <span>{COMMENTER_ID}</span>
         {comment}
       </div>
-      <img
-        alt="heart icon"
-        src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
-        className="commentItemLike"
-      />
+      <div>
+        <img
+          alt="heart icon"
+          src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+          className="commentItemLike"
+        />
+      </div>
     </li>
   );
 }
