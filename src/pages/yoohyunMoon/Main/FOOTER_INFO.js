e4861b48de58051b3020232ecaeede6c3089f4b5
yoohmoon <ansekfzhd5455@naver.com>
2023-04-26 16:39:21 +0900

    Data 1-3 : Aside에서 상수 데이터 활용 - 파일 분리

---
+++ src/pages/yoohyunMoon/Main/FOOTER_INFO.js
@@ -0,0 +1,24 @@
+export const FOOTER_INFO = [
+  { id: 1, src: 'https://about.instagram.com/', title: '소개' },
+  { id: 2, src: 'https://help.instagram.com/', title: '도움말' },
+  { id: 3, src: 'http://about.instagram.com/blog/', title: '홍보 센터' },
+  {
+    id: 4,
+    src: 'https://developers.facebook.com/docs/instagram',
+    title: 'API',
+  },
+  { id: 5, src: 'http://instagram.com/about/jobs/', title: '채용 정보' },
+  {
+    id: 6,
+    src: 'https://privacycenter.instagram.com/policy/?entry_point=ig_help_center_data_policy_redirect',
+    title: '개인정보처리방침',
+  },
+  { id: 7, src: 'https://help.instagram.com/581066165581870/', title: '약관' },
+  { id: 8, src: 'https://www.instagram.com/explore/locations/', title: '위치' },
+  { id: 9, src: 'https://about.instagram.com/', title: '언어' },
+  {
+    id: 10,
+    src: 'https://about.meta.com/technologies/meta-verified/',
+    title: 'Meta Verified',
+  },
+];
